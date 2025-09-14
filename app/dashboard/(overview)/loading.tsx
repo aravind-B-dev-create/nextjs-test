2dfc2c16b0129e5b9520bd728d3076f45b897025
Aravind <aravindn2008@gmail.com>
2025-09-13 22:36:03 -0700

    added middleware

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,12 @@
+import DashboardSkeleton from "../../ui/skeletons"
+
+
+export default async function Loading(){
+   return (
+       <html>
+        <body>
+            <DashboardSkeleton />
+        </body>
+       </html>
+   )
+}
